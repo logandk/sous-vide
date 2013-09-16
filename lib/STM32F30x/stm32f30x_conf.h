ff9516f0de6c012f1c2b4aac4be1a449c9465468
Logan Raarup <logan@logan.dk>
2013-09-16 18:29:07 +0200

    Added new libs

--- lib/STM32F30x/stm32f30x_conf.h
+++ lib/STM32F30x/stm32f30x_conf.h
@@ -34,6 +34,8 @@
 #include "stm32f30x_gpio.h"
 #include "stm32f30x_rcc.h"
 #include "stm32f30x_tim.h"
+#include "stm32f30x_exti.h"
+#include "stm32f30x_syscfg.h"
 #include "stm32f30x_misc.h"  /* High level functions for NVIC and SysTick (add-on to CMSIS functions) */
 
 /* Exported types ------------------------------------------------------------*/
